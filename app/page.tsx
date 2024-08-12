062e6e28bd454686086cedbaab82d039a314d5a7
mrvivekthumar <mrvivekthumar@gmail.com>
2024-08-12 19:56:29 +0530

    added different ways to use differnt font

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div>hello</div>
+        <div>
+            <h1 className="text-2xl">hello my name is vivek thumar</h1>
+            <h1 className="text-2xl font-pixel">hello my name is vivek thumar</h1>
+            <h1 className="font-oswald text-2xl">hello my name is vivek thumar</h1>
+        </div>
     );
 }

02783727ae3c0e8b0a3740946aa9267a6d66527d
mrvivekthumar <mrvivekthumar@gmail.com>
2024-08-14 19:40:38 +0530

    font view

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div>
-            <h1 className="text-2xl">hello my name is vivek thumar</h1>
-            <h1 className="text-2xl font-pixel">hello my name is vivek thumar</h1>
-            <h1 className="font-oswald text-2xl">hello my name is vivek thumar</h1>
+            <h1 className="text-2xl">This is vivek portfolio</h1>
+            <h1 className="text-2xl font-pixel">This is vivek portfolio</h1>
+            <h1 className="font-oswald text-2xl">This is vivek portfolio</h1>
         </div>
     );
 }

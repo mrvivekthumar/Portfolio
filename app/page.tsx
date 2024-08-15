fb329c654d3195a50698653f718f606a4b0e2ec7
mrvivekthumar <mrvivekthumar@gmail.com>
2024-08-15 19:00:52 +0530

    created card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 "use client";
+import Card from "@/components/ui/card";
 import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper";
 
 export default function Home() {
     return (
         <WaterWaveWrapper
             imageUrl=""
-            dropRadius="1"
+            dropRadius="3"
             perturbance="2"
             resolution="2048"
         >
-            {() => <div className="h-screen"></div>}
+            {() => <div className="h-screen p-10">
+                <div className="max-w-2xl mx-auto">
+                    <Card title="ui Component">...</Card>
+                </div>
+            </div>}
         </WaterWaveWrapper>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,35 @@ const config: Config = {
     ],
     theme: {
         extend: {
-            fontFamily:
-            {
-                oswald: ['var(--font-oswald)'],
-                pixel: ['var(--font-pixel)']
+            fontFamily: {
+                oswald: ["var(--font-oswald)"],
+                pixel: ["var(--font-pixel)"],
+            },
+            screens: { xs: "520px", "2xl": "1400px" },
+            colors: {
+                primary: {
+                    background: "rgba(255, 255, 255, .05)",
+                    foreground: "#fff",
+                },
+                secondary: {
+                    background: "rgba(255, 255, 255, .03)",
+                    foreground: "rgba(255,255,255,0.4)",
+                },
+                blue: {
+                    joust: "#56acff",
+                    cosmos: "#003049",
+                    hera: "#7179ef",
+                },
+                green: {
+                    benzol: "#00d372",
+                },
+                pink: {
+                    ice: "#d87cac",
+                },
+                border: "rgb(255 255 255 / 0.05)",
             },
             backgroundImage: {
-                grain: "url(/assets/images/background/noisy_background.jpg)",
+                grain: "url(/assets/images/background/noisy-background.png)",
             },
             animation: {
                 "noisy-bg": "noise 1s steps(2) infinite",
@@ -55,7 +77,7 @@ const config: Config = {
                         transform: "translate3d(-7rem, 0, 0)",
                     },
                 },
-            }
+            },
         },
     },
     plugins: [],

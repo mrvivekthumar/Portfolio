773a86186a5570bce379df4452d7ee16091d7f4c
mrvivekthumar <mrvivekthumar@gmail.com>
2024-08-18 14:57:58 +0530

    created email setup working using emailjs

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -5,16 +5,22 @@ interface InputPorps {
     icon?: ReactNode;
     placeholder: string;
     type: "email" | "text" | "password";
+    name: string;
 }
 
-const Input: FC<InputPorps> = ({ icon, placeholder, type }) => {
+const Input: FC<InputPorps> = ({ icon, placeholder, type, name }) => {
     return (
         <div className="relative w-full">
             {/* Icon */}
             <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                 {icon}
             </div>
-            <input type={type} placeholder={placeholder} className="w-full bg-primary-background text-primary-foreground rounded-lg text-sm ps-10 px-2.5 py-4 focus:outline-none" />
+            <input
+                type={type}
+                placeholder={placeholder}
+                name={name}
+                className="w-full bg-primary-background text-primary-foreground rounded-lg text-sm ps-10 px-2.5 py-4 focus:outline-none"
+            />
 
         </div>)
 }

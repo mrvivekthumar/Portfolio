bc2b1e6bf1d0c84464c42e44e17f5ca4f04e68db
mrvivekthumar <mrvivekthumar@gmail.com>
2024-08-18 18:49:39 +0530

    added card shade in profile navigation links

--- components/navigation/header/full-screen-menu/full-screen-menu.tsx
+++ components/navigation/header/full-screen-menu/full-screen-menu.tsx
@@ -4,6 +4,7 @@ import Curve from "./curve"
 import Profile from "@/components/ui/profile"
 import NavLink from "./nav-link"
 import Link from "next/link"
+import MenuCard from "./menu-card"
 
 export default function FullScreenMenu() {
     return (
@@ -13,25 +14,27 @@ export default function FullScreenMenu() {
             animate='enter'
             className="h-screen w-full bg-black fixed top-0 right-0 text-primary-foreground z-40 font-oswald "
         >
-            <div className="relative w-full max-w-[95%] mx-auto">
+            <div className="relative w-full pl-[5%] ">
                 {/* Profile */}
                 <div className="absolute top-8 ">
                     <Profile />
                 </div>
             </div>
             {/* Menu and card */}
-            <div className="absolute bottom-32  w-full lg:pl-[5%] top-36">
+            <div className="absolute bottom-32  w-full lg:px-[5%]">
                 <div className="grid relative" style={{ gridTemplateColumns: "1fr 500px" }}>
                     <div className="pl-4 flex flex-col justify-end ">
-                        {
-                            navItems.map((item, index) => (
-                                <NavLink
-                                    key={index}
-                                    data={{ ...item, index }}
-                                />
-                            ))
-                        }
+                        {navItems.map((item, index) => (
+                            <NavLink
+                                key={index}
+                                data={{ ...item, index }}
+                            />
+                        ))}
                     </div>
+
+
+                    {/* Menu about card */}
+                    <MenuCard />
                 </div>
             </div>
 

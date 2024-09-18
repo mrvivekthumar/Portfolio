642aa2a580b89add716e89a539045fca1bb664a8
mrvivekthumar <mrvivekthumar@gmail.com>
2024-09-19 03:00:45 +0530

    changed the images formate

--- components/cards/me.tsx
+++ components/cards/me.tsx
@@ -13,6 +13,7 @@ export default function MeCard() {
                     src={MyImg}
                     alt='Vivek Thumar Image'
                     className='absolute top-0 left-0 bottom-0 right-0 h-full w-full object-cover'
+
                 />
 
                 {/* Tags */}
--- data/gallery.ts
+++ data/gallery.ts
@@ -1,25 +1,30 @@
-import IMG from "@/public/assets/images/gallery/IMG.jpg"
-import IMG1 from "@/public/assets/images/gallery/IMG1.jpg"
-import IMG2 from "@/public/assets/images/gallery/IMG2.jpg"
-import IMG3 from "@/public/assets/images/gallery/IMG3.jpg"
+import Img1 from "@/public/assets/images/gallery/IMG.jpg";
+import Img2 from "@/public/assets/images/gallery/IMG1.jpg";
+import Img3 from "@/public/assets/images/gallery/IMG2.jpg";
+import Img4 from "@/public/assets/images/gallery/IMG3.jpg";
+import Img5 from "@/public/assets/images/gallery/IMG4.jpg";
 
 export const galleryImages = [
     {
         id: 0,
-        img: IMG,
+        img: Img1,
     },
     {
         id: 1,
-        img: IMG1,
+        img: Img2,
     },
     {
         id: 2,
-        img: IMG2,
+        img: Img3,
     },
     {
         id: 3,
-        img: IMG3,
+        img: Img4,
     },
-]
+    {
+        id: 4,
+        img: Img5,
+    },
+];
 
 export default galleryImages;
